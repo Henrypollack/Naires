e8b43e188504b4ffb6dac281908552baa95d5129
Antonio <antonio@MacBook-de-Antonio.local>
2024-06-18 20:04:41 -0300

    finalizando projeto pra ser usado no desktopagora e fianlizar o projeto para ser usado no mobile colocando tbm uma GUI

--- projeto.py
+++ projeto.py
@@ -1,18 +1,26 @@
 import datetime
 import os 
+import pprint
 dataAtt = datetime.date.today()
 mesAt = dataAtt.month
 anoAt = dataAtt.year
-        
+print(30*"\033[33m=\033[m")
+print("""\033[31m
+      
+      Seja Bem vindo!
+  Ao gerador de escala JR
+
+\033[m""")
+print(30*"\033[33m=\033[m")   
 class cabecalho:
     listaCabecalho = []
     def __init__(self, nomee):
        self.nomee = nomee 
     def inputCabecalho(self):
-        nomeUs = str(input("Digite o seu nome: "))
-        pix = str(input("Digite o seu pix: "))
-        local = str(input("Digite qual mercado voce trabalhou: "))
-        area = str(input("Digite qual area voce operou (Ex: Mercearia,Deposito,Corte de Frutas): "))
+        nomeUs = str(input("\033[32mDigite o seu nome: \033[m"))
+        pix = str(input("\033[32mDigite o seu pix: \033[m"))
+        local = str(input("\033[32mDigite qual mercado voce trabalhou: \033[m"))
+        area = str(input("\033[32mDigite qual area voce operou (Ex: Mercearia,Deposito,Corte de Frutas): \033[m"))
         cabecalho.listaCabecalho.append(f"*Nome:* _{nomeUs}_\n")
         cabecalho.listaCabecalho.append(f"*Pix:* _{pix}_\n")                                
         cabecalho.listaCabecalho.append(f"{local}\n")
@@ -31,15 +39,15 @@ class datas:
         
         self.nome = nome
     def inputDia(self):
-        DiasTra = int(input("Digite quantos dias voce trabalhou: "))
-        hrini =str(input("Digite a hora que voce entra(ex: 11:00,12:00): "))
-        hrfin = str(input("Digite a hora que voce sai(ex: 18:00,19:00): "))
+        DiasTra = int(input("\033[32mDigite quantos dias voce trabalhou: \033[m"))
+        hrini =str(input("\033[32mDigite a hora que voce entra(ex: 11:00,12:00): \033[m"))
+        hrfin = str(input("\033[32mDigite a hora que voce sai(ex: 18:00,19:00): \033[m"))
         mes = mesAt
         ano = anoAt
         i = 0
         while i != DiasTra:
             i += 1 
-            dia = int(input("Digite o dia: "))
+            dia = int(input("\033[32mDigite o dia trabalhado: \033[m"))
             datas.listaDatas.append(f"* {dia}/{mes}/{ano} - {hrini} as {hrfin}")
         self.DiasTra = DiasTra
         self.dia = dia
@@ -60,4 +68,14 @@ with open (nomeDoTXT,"w") as arquivo:
     arquivo.write("\n")
     for linha in datas.listaDatas:
         arquivo.write(linha + "\n")
+print("\n**Lista Cabecalho:**")
+if not cabecalho.listaCabecalho:
+    print("Lista vazia")
+else:
+    pprint.pprint(cabecalho.listaCabecalho)
 
+print("\n**Lista Datas:**")
+if not datas.listaDatas:
+    print("Lista vazia")
+else:
+    pprint.pprint(datas.listaDatas)

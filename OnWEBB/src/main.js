0ac533376fb9412493d76c66a4a986e803930fa8
Antonio <antonio@MacBook-de-Antonio.local>
2024-07-31 23:05:18 -0300

    colocando imagens de antes e depois

--- OnWEBB/src/main.js
+++ OnWEBB/src/main.js
@@ -1,3 +1,26 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var container = document.querySelector('.image-flip');
+    var images = document.querySelectorAll('.image-flip .flip-img');
+    var currentIndex = 0;
+
+    function adjustContainerHeight() {
+        // Ajusta a altura do contêiner com base na imagem visível
+        var visibleImage = images[currentIndex];
+        container.style.height = visibleImage.clientHeight + 'px';
+    }
+
+    function flipImages() {
+        images[currentIndex].classList.remove('show');
+        currentIndex = (currentIndex + 1) % images.length;
+        images[currentIndex].classList.add('show');
+        adjustContainerHeight();
+    }
+
+    images[currentIndex].classList.add('show');
+    adjustContainerHeight(); // Ajusta a altura inicial do contêiner
+    setInterval(flipImages, 6000);
+});
+
 document.addEventListener('DOMContentLoaded', function() {
     // Adiciona o primeiro campo de seleção de dias
     adicionarCampoDia();
